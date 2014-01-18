20da53ecfb80dc4581613e0ad74bedcbab805ad8
takedamasaki555 <masakit@microsoft.com>
2014-01-18 14:33:37 +0900

    Update index.php
    test

--- index.php
+++ index.php
@@ -7,16 +7,14 @@
     <body>
 <?php
 $baseurl = 'http://mtdevcs01.cloudapp.net/mt/mt-data-api.cgi/v1/sites/1/entries';
-
 $json = file_get_contents($baseurl);
-
 $output = json_decode($json);
-print "<pre>";
-print_r($output);
-print "</pre>";
-
-
+foreach ( $output->items as $data ){
+    echo "<h1>タイトル<br/>".$data->title."</h1><br/>";
+    echo "<h1>内容".$data->body."</h1><br/><hr/>";
+}
 ?>
         
     </body>
 </html>
+
